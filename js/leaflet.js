f673914ea32759e8231d34427d83aca69a630eb0
Alessandro <alex.carboni12@gmail.com>
2019-01-25 00:17:15 +0100

    Choropleth map

--- js/leaflet.js
+++ js/leaflet.js
@@ -11,23 +11,23 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 // Import JSON
 d3.json("./countries/countries.geo.json", function(countriesData) {
     // Import csv
-    d3.csv("./countries/population-databank.csv", function (population) {
+    d3.csv("./countries/population-databank.csv", function (populationData) {
        
 
 
         // Prendere id da CSV
-        // population[0]["Country Code"]
+        // populationData[0]["Country Code"]
 
         // Prendere id da geoJSON
         // countriesData.features[i].id
 
-        
-        console.log(population.length);
+        console.log(populationData);
+        console.log(populationData.length);
         
         let idGeoJson = countriesData.features[0].properties.name;
         console.log(idGeoJson);
         
-        let idCSV = population[0]["Country Code"];
+        let idCSV = populationData[0]["Country Code"];
         console.log(idCSV);
 // DA TOGLIERE
 
@@ -64,7 +64,7 @@ d3.json("./countries/countries.geo.json", function(countriesData) {
         // method that we will use to update the control based on feature properties passed
         info.update = function (props) {
             this._div.innerHTML = '<h4>World Population</h4>' + (props ?
-            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+            '<b>' + props.features.properties.name + '</b><br />' + props.density + ' people'
             : 'Hover over a country');
         };
 
@@ -76,44 +76,55 @@ d3.json("./countries/countries.geo.json", function(countriesData) {
 
 
         // Choropleth Test
-
-        //population[0][2017]
-
-        function countryColor (population) {
-            if ( population > 50000000000 ) {
+        function getColor(population) {
+            if (population > 10000000) { // Cento milioni
                 return '#006d2c'
-            } else if ( population > 25000000000 ) {
+            } else if (population > 50000000) { // 50 milioni
                 return '#2ca25f'
-            } else if ( population > 10000000000 ) {
+            } else if (population > 10000000) { // 10 milioni
                 return '#66c2a4'
-            } else if ( population> 5000000 ) {
+            } else if (population > 1000000) { // Un milione
                 return '#b2e2e2'
-            } else {
+            } else if (population > 0) {
                 return '#edf8fb'
+            } else {
+                return "transparent"
             }
         }
+        
+        // for (let i = 0; i < populationData.length; i++) {
+        //     let countryColor = populationData[i][2017];
+        //     console.log (countryColor);
+        //     return countryColor
+        // }
 
-        for (let i = 0; i < population.length; i++) {
-            // Countries style
-            function countriesStyle (feature){
-                return {
-                    fillColor: countryColor(population[i][2017]),
-                    weight: 2,
-                    opacity: 1,
-                    fillOpacity: 0.7
-                }
+        
+        
+        // Countries style
+        function countryStyle(feature) {
+            //console.log(feature);
+            var countryId = feature.id,
+                countryData = populationData.filter(function(d) { return d['Country Code'] === countryId; }), // Il metodo filter degli array ritorna un array dei soli elementi che soddisfano l'uguaglianza: https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+                countryPopulation = 0;
+
+            if (countryData.length > 0) { // Hai trovato una corrispondenza
+                countryPopulation = parseInt(countryData[0]['2017']); // Prendo il primo e unico elemento trovato, prendo il valore del 2017 e lo faccio diventare un numero
+            } else {
+                countryPopulation = 0; // Assenza del dato = popolazione uguale a 0
             }
-            
-            
+
+            return {
+                fillColor: getColor(countryPopulation),
+                weight: 2,
+                opacity: 1,
+                fillOpacity: 0.7
+            }
         }
-
-
-
-
+        
         // Map interaction
         function highlightFeature(e) {
             var layer = e.target;
-        
+            
             layer.setStyle({
                 weight: 5,
                 color: '#fff',
@@ -147,12 +158,18 @@ d3.json("./countries/countries.geo.json", function(countriesData) {
                 mouseout: resetHighlight,
                 click: zoomToFeature
             });
+            // Prova ad aggiungere qui un popup che mostri il nome del paese e la sua popolazione...
+            // Vedi https://leafletjs.com/examples/geojson/ (capitolo onEachFeature)
+            // Occhio che il popup si apre al click sul paese e tu già sopra gli dici di zoomare...
+            // La popolazione la ricavi di nuovo come fatto prima nella funzione countryStyle
         }
-        
+
         geojson = L.geoJson(
             countriesData,
-            { style: countriesStyle },
-            { onEachFeature: onEachFeature },
+            {
+                onEachFeature: onEachFeature,
+                style: countryStyle // Dal tutorial: https://leafletjs.com/examples/choropleth/ (capitolo Adding Some Color, secondo blocco di codice)
+            },
         ).addTo(mymap);
 
 
